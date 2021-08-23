f49f838d022f168a9adec5f4dd113b9ea1ed8313
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-08-23 13:37:33 +0530

    Basic

---
+++ public/js/main.js
@@ -0,0 +1,212 @@
+class TodoItem {
+	constructor(todo) {
+		this.todo = todo;
+		this.isCompleted = false;
+	}
+}
+
+let todoData = JSON.parse(localStorage.getItem('persistantData')) ?? [];
+
+const inputBox = document.getElementById('inputBox');
+const btnAdd = document.getElementById('btnAdd');
+const listData = document.getElementById('listData');
+
+inputBox.addEventListener('keydown', ({ key }) => {
+	if (key === 'Enter') {
+		if (!inputBox.value.trim()) {
+			alert('Enter something to add to your list!');
+			return;
+		}
+		let todoItem = new TodoItem(inputBox.value);
+		addTodo();
+		inputBox.value = '';
+	}
+});
+
+btnAdd.addEventListener('click', addTodo);
+
+function addTodo() {
+	if (!inputBox.value.trim()) {
+		alert('Enter something to add to your list!');
+		return;
+	}
+	let todoItem = new TodoItem(inputBox.value);
+	todoData.push(todoItem);
+	inputBox.value = '';
+	refresh();
+}
+
+{
+	/* <div class="bg-gray-300 rounded-md shadow-lg p-2">
+                    <div class="flex justify-between px-2">
+                        <div>
+                            todo Item 1
+                        </div>
+                        <div class="flex space-x-1 text-gray-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 10l7-7m0 0l7 7m-7-7v18" />
+                            </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+                            </svg>
+
+                        </div>
+                    </div>
+                </div>
+                <!--Card Ends here-- > */
+}
+
+function refresh() {
+	localStorage.setItem('persistantData', JSON.stringify(todoData));
+	if (todoData.length > 0) {
+		// has data
+		let html = '';
+		for ([index, todo] of todoData.entries()) {
+			if (todo.isCompleted) {
+				// if is completed
+				html += `
+                <div class="bg-green-300 rounded-md shadow-lg p-2">
+                <div class="flex justify-between px-2">
+                    <div class="w-64 font-poppins">
+                        ${todo.todo}
+                    </div>
+                    <div class="flex space-x-1 text-green-900 items-center">
+                        <div id="undocompleteTodo" onClick="undocompleteTodo(${index});" class="cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg></div>
+
+                        <div id="deleteTodo" onClick="deleteTodo(${index});" class="cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg></div>
+
+                        <div id="moveUp" onClick="moveItemUp(${index});" class="cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M5 10l7-7m0 0l7 7m-7-7v18" />
+                        </svg></div>
+
+                        <div id="moveDown" onClick="moveItemDown(${index});" class="cursor-pointer">
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+                        </svg></div>
+
+                    </div>
+                </div>
+            </div>
+                `;
+			} else {
+				html += `
+            <div class="bg-gray-300 rounded-md shadow-lg p-2">
+                    <div class="flex justify-between px-2">
+                        <p class="w-64 font-poppins">
+                            ${todo.todo}
+                        </p>
+                        <div class="flex space-x-1 text-gray-800 items-center">
+                            <div id="completeTodo" onClick="completeTodo(${index});" class="cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg></div>
+
+                            <div id="deleteTodo" onClick="deleteTodo(${index});" class="cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg></div>
+
+                            <div id="moveUp" onClick="moveItemUp(${index});" class="cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 10l7-7m0 0l7 7m-7-7v18" />
+                            </svg></div>
+
+                            <div id="moveDown" onClick="moveItemDown(${index});" class="cursor-pointer">
+                            
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+                            </svg></div>
+
+                        </div>
+                    </div>
+                </div>
+            `;
+			}
+		}
+
+		listData.innerHTML = html;
+	} else {
+		listData.innerHTML = `<h1>Your todo list is empty..</h1>`;
+	}
+}
+
+function completeTodo(currentIndex) {
+	todoData[currentIndex].isCompleted = true;
+	refresh();
+}
+
+function undocompleteTodo(currentIndex) {
+	todoData[currentIndex].isCompleted = false;
+	refresh();
+}
+
+function deleteTodo(currentIndex) {
+	console.log('delete');
+	todoData = todoData.filter((item, index) => {
+		if (currentIndex != index) return item;
+	});
+	refresh();
+}
+
+function moveItemUp(currentIndex) {
+	if (currentIndex == 0) {
+		return;
+	}
+	let temp = todoData[currentIndex - 1];
+	todoData[currentIndex - 1] = todoData[currentIndex];
+	todoData[currentIndex] = temp;
+	refresh();
+	console.log(currentIndex);
+	console.log(currentIndex);
+}
+
+function moveItemDown(currentIndex) {
+	if (currentIndex == todoData.length - 1) {
+		return;
+	}
+	let temp = todoData[currentIndex + 1];
+	todoData[currentIndex + 1] = todoData[currentIndex];
+	todoData[currentIndex] = temp;
+	refresh();
+	console.log(currentIndex);
+}
+
+refresh();
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				header: ['Patua One; cursive'],
+				poppins: ['Poppins']
+			}
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
